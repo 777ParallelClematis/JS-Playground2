b3eba3c289073f2e3322c1fa5566960e1b5d58c0
777ParallelClematis <erin-colane@live.nmit.ac.nz>
2024-03-07 15:01:37 +1300

    got a little tangled toward the end

--- numbers.js
+++ numbers.js
@@ -1,11 +1,57 @@
 //NOTE - 
 /*var is an older version (es5) of let*/
-const num1 = 10
-console.log(num1)
+const pi = 3.141
+//pi = 10
+console.log(pi)
 
 let num2 = 20
 console.log(num2)
+num2 = 25
 
 var num3 = 30
 console.log(num3)
-//
+
+const fullName = "Erin Colane"
+console.log(fullName)
+
+const workplace = "NMIT"
+console.log(workplace)
+
+console.log(fullName + " " + workplace)
+console.log(fullName + workplace)
+
+console.log(`My full name is ${fullName} and I work at ${workplace}`)
+
+const working = true
+const notworking = false
+
+if (working) {
+    console.log(fullName)
+}
+
+let a = 2
+let result 
+// if() {} else {}
+
+// if (a > 0) {
+//     result = 'positive';
+// console.log(result)
+// } else {
+//     result = 'NOT positive'
+//     console.log(result)
+// }
+
+
+
+let a = 2
+let result
+// if() {} else {}
+a >= 0
+
+if (a > 0) {
+    result = 'positive';
+console.log(result)
+} else {
+    result = 'NOT positive'
+    console.log(result)
+}
